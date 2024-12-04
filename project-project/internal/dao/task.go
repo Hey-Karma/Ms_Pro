78bfc324346a808f3a52d56e4a43bb334a004c1a
Yzt <Howard_Yangzt@163.com>
2024-12-04 15:47:13 +0800

    任务-创建任务

--- project-project/internal/dao/task.go
+++ project-project/internal/dao/task.go
@@ -4,11 +4,42 @@ import (
 	"context"
 	"gorm.io/gorm"
 	"test.com/project-project/internal/data"
+	"test.com/project-project/internal/database"
 	"test.com/project-project/internal/database/gorms"
 )
 
 type TaskDao struct {
 	conn *gorms.GormConn
+}
+
+func (t TaskDao) SaveTask(ctx context.Context, conn database.DbConn, ts *data.Task) error {
+	t.conn = conn.(*gorms.GormConn)
+	err := t.conn.Tx(ctx).Save(&ts).Error
+	return err
+}
+
+func (t TaskDao) SaveTaskMember(ctx context.Context, conn database.DbConn, tm data.TaskMember) error {
+	t.conn = conn.(*gorms.GormConn)
+	err := t.conn.Tx(ctx).Save(&tm).Error
+	return err
+}
+
+func (t TaskDao) FindTaskSort(ctx context.Context, projectCode int64, stageCode int64) (v *int, err error) {
+	session := t.conn.Session(ctx)
+	// select * from 其中的*的部分如果使用聚合函数一定要用Scan而不是Find，Find是将整个模型放入，Scan是放入聚合函数的值映射到非模型变量中
+	err = session.Model(&data.Task{}).
+		Where("project_code=? and stage_code=?", projectCode, stageCode).
+		Select("max(sort)").Scan(&v).Error
+	return
+}
+
+func (t TaskDao) FindTaskMaxIdNum(ctx context.Context, projectCode int64) (v *int, err error) {
+	session := t.conn.Session(ctx)
+	// select * from 其中的*的部分如果使用聚合函数一定要用Scan而不是Find，Find是将整个模型放入，Scan是放入聚合函数的值映射到非模型变量中
+	err = session.Model(&data.Task{}).
+		Where("project_code=?", projectCode).
+		Select("max(id_num)").Scan(&v).Error
+	return
 }
 
 func (t TaskDao) FindTaskMemberByTaskId(ctx context.Context, taskCode int64, memberId int64) (task *data.TaskMember, err error) {
